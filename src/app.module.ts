33d92c3828a7118e5e6e64b05e6d15bb173cb5f4
IankaLps <iankalp50@gmail.com>
2025-01-22 13:23:20 -0300

    :wrench: chore: Configuração de importações

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Usuario } from './usuario/entities/usuario.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host:'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_crm',
+      entities: [Usuario],
+      synchronize: true,
+      logging: true,
+    }),
+    UsuarioModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
