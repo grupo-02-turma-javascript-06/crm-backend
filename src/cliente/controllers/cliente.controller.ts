ae6e00f71a9a2910024096801c4d2b45b46bd39d
junoprice <jonas.3636@hotmail.com>
2025-01-22 13:31:39 -0300

    :sparkles: feat: Criação do CRUD da entidade Cliente

---
+++ src/cliente/controllers/cliente.controller.ts
@@ -0,0 +1,45 @@
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
+import { ClienteService } from "../services/cliente.service";
+import { Cliente } from "../entities/cliente.entity";
+
+@Controller("/Cliente")
+export class ClienteController {
+    constructor(
+        private readonly clienteService: ClienteService){ }
+
+    @Get("/:id")
+    @HttpCode(HttpStatus.OK)
+    findbyId(@Param("id", ParseIntPipe) id: number): Promise<Cliente> {
+        return this.clienteService.findById(id);
+    }
+
+    @Get("/all")
+    @HttpCode(HttpStatus.OK)
+    findAll(): Promise<Cliente[]>{
+        return this.clienteService.findAll();
+    }
+
+    @Get("/cliente/:cliente")
+    @HttpCode(HttpStatus.OK)
+    findByCliente(@Param("cliente") cliente: string): Promise<Cliente[]> {
+        return this.clienteService.findByCliente(cliente);
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() cliente : Cliente): Promise<Cliente> {
+        return this.clienteService.create(cliente);
+    }
+
+    @Put()
+    @HttpCode(HttpStatus.OK)
+    update(@Body() cliente : Cliente): Promise<Cliente> {
+        return this.clienteService.update(cliente)
+    }
+
+    @Delete("/:id")
+    @HttpCode(HttpStatus.NO_CONTENT)
+    delete(@Param("id" , ParseIntPipe) id: number){
+        return this.clienteService.delete(id);
+    }
+}
